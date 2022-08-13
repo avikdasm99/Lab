03a5d060b199dfc177c4a9027d208164ac307747
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-08-13 12:00:48 +0530

    Add files via upload

---
+++ Check_Java_Is_installornot.java
@@ -0,0 +1,19 @@
+class Check_Java_Is_installornot{
+
+public static void main(String ...args){
+
+
+System.out.println( "Java Version : "+System.getProperty("java.version"));
+
+System.out.println( "Java Runtime Version : "+System.getProperty("java.runtime.version"));
+
+System.out.println( "Java Home : "+System.getProperty("java.home"));
+
+System.out.println( "Java Vendor : "+System.getProperty("java.vendor"));
+
+System.out.println( "Java Class Path :  "+System.getProperty("java.class.path"));
+
+
+
+}
+}
