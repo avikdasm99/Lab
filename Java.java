d3874955ed3a97f7422e44d0738b0db082ed5919
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-09-08 18:36:21 +0530

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,14 @@
+/*Q.3... Develop a public java class and make sure nobody can create
+any object of this class from outside the class*/
+
+package sep8;
+
+public class Java {
+	
+	private Java(){
+		
+	}
+	public static void main(String[] args) {
+		
+	}
+}
