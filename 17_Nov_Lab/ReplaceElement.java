9c28c55ca63e0d3fb89c203ec3795045543c3e10
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-12-08 15:20:28 +0530

    Update ReplaceElement.java

--- 17_Nov_Lab/ReplaceElement.java
+++ 17_Nov_Lab/ReplaceElement.java
@@ -2,14 +2,14 @@
 in a linked list.*/
 
 package lab10;
-import java.util.*;
+import java.util.*;   // import package
 public class ReplaceElement {
 
-	public static void main(String[] args) {
-		LinkedList<String> name=new LinkedList<String>();
-		Collections.addAll(name, "Avik","Biplob","Soumen","Ratan");
-		System.out.println("before replace="+name);
-		name.set(0, "Krishnendu");
-		System.out.println("after replace="+name);
+	public static void main(String[] args) {   // main method
+		LinkedList<String> name=new LinkedList<String>();  // create LinkedList
+		Collections.addAll(name, "Avik","Biplob","Soumen","Ratan");  // adding elements
+		System.out.println("before replace="+name); // showing elements before replace
+		name.set(0, "Krishnendu"); // replace elements at position 1
+		System.out.println("after replace="+name); // showing elements after replace
 	}
 }
