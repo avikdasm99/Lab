203ca0a1076b390e59260999feae3ef1aeaa51f7
avikdasm99 <110466059+avikdasm99@users.noreply.github.com>
2022-12-08 15:25:17 +0530

    Update CompareArraylist.java

--- 17_Nov_Lab/CompareArraylist.java
+++ 17_Nov_Lab/CompareArraylist.java
@@ -4,14 +4,14 @@ package lab10;
 import java.util.*;
 public class CompareArraylist {
 
-	public static void main(String[] args) {
-		ArrayList<String> color1=new ArrayList<String>();
-		Collections.addAll(color1, "red","green","black","white","pink");
-		ArrayList<String> color2=new ArrayList<String>();
-		Collections.addAll(color2, "red","green","black","pink");
-		ArrayList<String> result = new ArrayList<String>();
+	public static void main(String[] args) {   // main method
+		ArrayList<String> color1=new ArrayList<String>();   // create ArrayList color1
+		Collections.addAll(color1, "red","green","black","white","pink");   //adding elements to color1
+		ArrayList<String> color2=new ArrayList<String>();  // create ArrayList color2
+		Collections.addAll(color2, "red","green","black","pink");  //adding elements to color1
+		ArrayList<String> result = new ArrayList<String>(); // create ArrayList Result where the compare values are stored using For loop
         for (String s : color1)
         	result.add(color2.contains(s) ? "yes" : "no");
-        System.out.println(result);
+        System.out.println(result);  // show the result Arraylist
 	}
 }
